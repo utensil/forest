8dc52eb1df7e2fcbfc8fba8ba560c52731afd2c9
utensil <utensilcandel@gmail.com>
2024-05-05 13:56:57 +0800

    Fix fize and apply, add pize

--- fize.sh
+++ fize.sh
@@ -2,18 +2,20 @@
 
 # foresterize a tree with LaTeX math
 
-$TREE = $1
+TREE="trees/$1.tree"
 
 # for the file $TREE, replace all string matching regrex \$([^$]+)\$ to #{$1} where $1 is the first match using sed inplace
-sed -i '' -E 's/\$([^$]+)\$/#{$1}/g' $TREE
+sed -i '' -E 's/\$([^$]+)\$/#{\1}/g' $TREE
 # for the file $TREE, replace all string matching regrex \$\$\n([^$]+)\$\$ to ##{$1} where $1 is the first match using sed inplace
-sed -i '' -E 's/\$\$\n([^$]+)\$\$/##{$1}/g' $TREE
+cat $TREE | tr '\n' '\r' | sed -E 's/\$\$(\r[^$]+)\$\$/##{\1}/g' > $TREE.tmp
+cat $TREE.tmp | tr '\r' '\n' > $TREE
+rm $TREE.tmp
 # for the file $TREE, replace all string \( to #{ using sed inplace
-sed -i '' -E 's/\\(/#{/g' $TREE
+sed -i '' -E 's/\\\(/#\{/g' $TREE
 # for the file $TREE, replace all string \) to } using sed inplace
-sed -i '' -E 's/\\)/}/g' $TREE
+sed -i '' -E 's/\\\)/}/g' $TREE
 # for the file $TREE, replace all string \[ to ##{ using sed inplace
-sed -i '' -E 's/\\\[/##{/g' $TREE
+sed -i '' -E 's/\\\[/##\{/g' $TREE
 # for the file $TREE, replace all string \] to } using sed inplace
 sed -i '' -E 's/\\\]/}/g' $TREE
 # for the file $TREE, replace \texdef to \refdef using sed inplace
