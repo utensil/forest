fb9c090d6e6b1a5e312ddf4e6727f60b54d4ae49
utensil <utensilcandel@gmail.com>
2024-05-22 22:24:42 +0800

    watch and web server

--- watch.sh
+++ watch.sh
@@ -5,5 +5,9 @@ rm -rf build
 rm -rf output
 ./build.sh
 
-watchexec --no-vcs-ignore --project-origin . --on-busy-update queue --poll 500ms -e tree,tex,css,js,xsl -w trees -w assets --emit-events-to=stdio -- ./build_changed.sh
+watchexec --no-vcs-ignore --project-origin . --on-busy-update queue --poll 500ms -e tree,tex,css,js,xsl -w trees -w assets --emit-events-to=stdio -- ./build_changed.sh &
+
+http-server -p 1314 output &
+
+wait
 
