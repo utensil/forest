56dda2fce0cec1371d43b892effd2a1c7c91591b
utensil <utensilcandel@gmail.com>
2024-06-02 17:51:18 +0800

    feat(ca): add operations

--- fize.sh
+++ fize.sh
@@ -17,8 +17,9 @@ sed -i '' -E 's/\$([^$]+)\$/#{\1}/g' $TREE
 cat $TREE | tr '\n' '\r' | sed -E 's/\$\$([^$]+)\$\$/##{\1}/g' > $TREE.tmp
 # 1. \texdef{}{}{ -> \refdef{}{}{\r\\p{
 sed -i '' -E 's/\\texdef\{([^\}]*)\}\{([^\}]*)\}\{/\\refdef\{\1\}\{\2\}\{\n\\p\{/g' $TREE.tmp
+sed -i '' -E 's/\\texnote\{([^\}]*)\}\{([^\}]*)\}\{/\\refnote\{\1\}\{\2\}\{\n\\p\{/g' $TREE.tmp
 # 2. before the line containing \refdef, skip; after the line, replace \r\r -> }\r\r\\p{ 
-awk 'BEGIN {skip=1} {if ($0 ~ /\\refdef/) {skip=0;print $0} else if (skip==1) {print $0} else { gsub(/\r\r/,"}\r\r\\p{", $0); print $0} }' $TREE.tmp > $TREE.tmp2
+awk 'BEGIN {skip=1} {if ($0 ~ /\\(refdef|refnote)/) {skip=0;print $0} else if (skip==1) {print $0} else { gsub(/\r\r/,"}\r\r\\p{", $0); print $0} }' $TREE.tmp > $TREE.tmp2
 # 3. }\s*$ -> }}
 sed -i '' -E 's/\}\s*$/\}\}\r/g' $TREE.tmp2
 cat $TREE.tmp2 | tr '\r' '\n' > $TREE
@@ -32,5 +33,5 @@ sed -i '' -E 's/\\\[/##\{/g' $TREE
 # for the file $TREE, replace all string \] to } using sed inplace
 sed -i '' -E 's/\\\]/}/g' $TREE
 # for the file $TREE, replace \texdef to \refdef using sed inplace
-sed -i '' -E 's/\\texdef/\\refdef/g' $TREE
+# sed -i '' -E 's/\\texdef/\\refdef/g' $TREE
 
