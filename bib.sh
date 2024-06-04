3809efd62ebe29132e4cf07ae4395a59d5be0cfe
utensil <utensilcandel@gmail.com>
2024-06-04 10:28:56 +0800

    No cache, override

--- bib.sh
+++ bib.sh
@@ -2,14 +2,10 @@
 
 # update and regenerate bibliography trees
 
-curl https://raw.githubusercontent.com/utensil/blog/main/content/posts/transformer/bib.bib  -o trees/refs/transformer.bib
-
-curl https://raw.githubusercontent.com/utensil/blog/main/content/posts/math-2024/bib.bib  -o trees/refs/math-2024.bib
-
-curl https://raw.githubusercontent.com/utensil/blog/main/content/posts/zeon-algebra/bib.bib  -o trees/refs/zeon-algebra.bib
-
-curl https://raw.githubusercontent.com/utensil/lean-ga/blueprint/blueprint/src/references.bib -o trees/refs/lean-ga.bib
-
+curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/transformer/bib.bib  -o trees/refs/transformer.bib
+curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/math-2024/bib.bib  -o trees/refs/math-2024.bib
+curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/zeon-algebra/bib.bib  -o trees/refs/zeon-algebra.bib
+curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/lean-ga/blueprint/blueprint/src/references.bib -o trees/refs/lean-ga.bib
 
 python split_bib.py transformer
 python split_bib.py math-2024
