992d8e98af5429b44eba79c62629dae2e06248cb
utensil <utensilcandel@gmail.com>
2024-06-04 09:43:46 +0800

    Add bib.sh

---
+++ bib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# update and regenerate bibliography trees
+
+curl https://raw.githubusercontent.com/utensil/blog/main/content/posts/transformer/bib.bib  -o trees/refs/transformer.bib
+
+curl https://raw.githubusercontent.com/utensil/blog/main/content/posts/math-2024/bib.bib  -o trees/refs/math-2024.bib
+
+curl https://raw.githubusercontent.com/utensil/blog/main/content/posts/zeon-algebra/bib.bib  -o trees/refs/zeon-algebra.bib
+
+curl https://raw.githubusercontent.com/utensil/lean-ga/blueprint/blueprint/src/references.bib -o trees/refs/lean-ga.bib
+
+
+python split_bib.py transformer
+python split_bib.py math-2024
+python split_bib.py zeon-algebra
+python split_bib.py lean-ga
+python split_bib.py forest
+
+
+
