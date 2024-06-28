358027bc6a491244882b69568b639ab0cbb7ac2f
utensil <utensilcandel@gmail.com>
2024-06-28 15:49:07 +0800

    Improve lost note detection

--- lost.sh
+++ lost.sh
@@ -11,19 +11,16 @@ for f in trees/*.tree; do
     # extract the filename without the extension
     FILENAME=$(basename $f .tree)
     # if FILENAME is not included in any files trees/*.tree
-    if ! grep -q $FILENAME trees/*.tree; then
+    # echo "^\\\\transclude\\{$FILENAME\\}"
+    if ! grep -q -E "^\\\\transclude\\{$FILENAME\\}" trees/*.tree; then
         # if "\tag{draft}" is not in the FILENAME
         if ! grep -q -F "\tag{draft}" $f; then
             # if FILENAME is not started with "math-"
-            if [[ $FILENAME != math-* ]]; then
-                if [[ $FILENAME != *-0001 ]]; then
-                    # if FOUND_LOST_NOTE == 0
-                    if [[ $FOUND_LOST_NOTE == 0 ]]; then
-                        echo
-                    fi
-                    FOUND_LOST_NOTE+=1
-                    echo "trees/$FILENAME.tree might be a lost note"
-                fi
+            if [[ $FILENAME != math-* && $FILENAME != *-0001 && $FILENAME != *macros && $FILENAME != index && $FILENAME != latex-preamble ]]; then
+                # add 1 to FOUND_LOST_NOTE
+                FOUND_LOST_NOTE=$((FOUND_LOST_NOTE + 1))
+                echo
+                echo "trees/$FILENAME.tree might be a lost note"
             fi
         fi
     fi
