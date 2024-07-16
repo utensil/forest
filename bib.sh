ad80e198b16e7f5213e7389b9888b8637e9823b8
utensil <utensilcandel@gmail.com>
2024-07-16 20:23:14 +0800

    Add pitts2001categorical

--- bib.sh
+++ bib.sh
@@ -3,10 +3,16 @@ set -euxo pipefail
 
 # update and regenerate bibliography trees
 
-curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/transformer/bib.bib  -o tex/transformer.bib
-curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/math-2024/bib.bib  -o tex/math-2024.bib
-curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/zeon-algebra/bib.bib  -o tex/zeon-algebra.bib
-curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/lean-ga/blueprint/blueprint/src/references.bib -o tex/lean-ga.bib
+BIBALL=${BIBALL:-}
+if [ -n "$BIBALL" ]; then
+    echo update all bib files including the ones in remote repos
+    curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/transformer/bib.bib  -o tex/transformer.bib
+    curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/math-2024/bib.bib  -o tex/math-2024.bib
+    curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/blog/main/content/posts/zeon-algebra/bib.bib  -o tex/zeon-algebra.bib
+    curl --clobber -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/utensil/lean-ga/blueprint/blueprint/src/references.bib -o tex/lean-ga.bib
+else
+    echo update only local bib files
+fi
 
 python split_bib.py transformer
 python split_bib.py math-2024 
