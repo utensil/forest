874f3c5bacea2d007204f55379b7773eb4af94e0
utensil <utensilcandel@gmail.com>
2024-07-22 17:55:58 +0800

    Add partial translation of Bourbaki on Clifford algebras

--- build.sh
+++ build.sh
@@ -44,8 +44,8 @@ function lize {
   show_lize_result ca-0001
   ./lize.sh tt-0001 2>&1 > /dev/null
   show_lize_result tt-0001
-  ./lize.sh math-0001 2>&1 > /dev/null
-  show_lize_result math-0001
+#   ./lize.sh math-0001 2>&1 > /dev/null
+#   show_lize_result math-0001
 }
 
 echo "⭐ Rebuilding forest"
