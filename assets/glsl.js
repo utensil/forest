f7b66767b0ca8f2363f8e968f919ef7e5aa859d1
utensil <utensilcandel@gmail.com>
2024-08-16 16:53:43 +0800

    Make both lygia and local includes work

--- assets/glsl.js
+++ assets/glsl.js
@@ -1,5 +1,43 @@
 import { ImageEffectRenderer } from 'https://esm.sh/@mediamonks/image-effect-renderer@2.4.0'
-import resolveLygia from "https://lygia.xyz/resolve.esm.js"
+// import resolveLygia from "https://lygia.xyz/resolve.esm.js"
+
+function getFile(url) {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url, false);
+    httpRequest.send();
+    if (httpRequest.status == 200)
+        return httpRequest.responseText;
+    else
+        return "";
+  }
+
+function resolveLygia(lines) {
+    if ( !Array.isArray(lines) ) {
+        lines = lines.split(/\r?\n/);
+    }
+
+    let src = "";
+    lines.forEach( (line, i) => {
+        const line_trim = line.trim();
+        if (line_trim.startsWith('#include "lygia') ) {
+            let include_url = line_trim.substring(15);
+            include_url = "https://lygia.xyz" + include_url.replace(/\"|\;|\s/g,'');
+            src += getFile(include_url) + '\n';
+        }
+        // uts begin
+        else if (line_trim.startsWith('#include "') ) {
+            let include_url = line_trim.substring(10);
+            include_url = include_url.replace(/\"|\;|\s/g,'');
+            src += getFile(include_url) + '\n';
+        }
+        // uts end
+        else {
+            src += line + '\n';
+        }
+    });
+    
+    return src;
+}
 
 // const shader = `
 // `;
