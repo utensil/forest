9a1b432a757f71ed0bdeba10fc5295cb8457dbb4
utensil <utensilcandel@gmail.com>
2024-08-17 15:52:46 +0800

    Switch to async resolution of shader includes

--- assets/shader.js
+++ assets/shader.js
@@ -4,42 +4,34 @@ import { ImageEffectRenderer } from 'https://esm.sh/@mediamonks/image-effect-ren
 
 // import resolveLygia from "https://lygia.xyz/resolve.esm.js"
 
-function getFile(url) {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", url, false);
-    httpRequest.send();
-    if (httpRequest.status == 200)
-        return httpRequest.responseText;
-    else
-        return "";
-  }
-
-function resolveLygia(lines) {
-    if ( !Array.isArray(lines) ) {
+async function resolveIncludesAsync(lines) {
+    if (!Array.isArray(lines))
         lines = lines.split(/\r?\n/);
-    }
-
+  
     let src = "";
-    lines.forEach( (line, i) => {
-        const line_trim = line.trim();
-        if (line_trim.startsWith('#include "lygia') ) {
-            let include_url = line_trim.substring(15);
-            include_url = "https://lygia.xyz" + include_url.replace(/\"|\;|\s/g,'');
-            src += getFile(include_url) + '\n';
-        }
-        // uts begin
-        else if (line_trim.startsWith('#include "') ) {
-            let include_url = line_trim.substring(10);
-            include_url = include_url.replace(/\"|\;|\s/g,'');
-            src += getFile(include_url) + '\n';
-        }
-        // uts end
-        else {
-            src += line + '\n';
-        }
-    });
-    
-    return src;
+    const response = await Promise.all(
+        lines.map(async (line, i) => {
+            const line_trim = line.trim();
+            if (line_trim.startsWith('#include "lygia')) {
+                let include_url = line_trim.substring(15);
+                include_url = "https://lygia.xyz" + include_url.replace(/\"|\;|\s/g, "");
+                console.debug("fetching", include_url);
+                return fetch(include_url).then((res) => res.text());
+            }
+            // uts begin
+            else if (line_trim.startsWith('#include "') ) {
+                let include_url = line_trim.substring(10);
+                include_url = include_url.replace(/\"|\;|\s/g,'');
+                console.debug("fetching", include_url);
+                return fetch(include_url).then((res) => res.text());
+            }
+            // uts end
+            else
+                return line;
+        })
+    );
+  
+    return response.join("\n");
 }
 
 // uts end
@@ -51,8 +43,8 @@ const embeded_shaders = document.querySelectorAll('.embeded-shader');
 embeded_shaders.forEach((element) => {
     let shader = element.textContent;
     element.textContent = '';
-    
-    shader = resolveLygia(shader);
-    // console.log(shader.substring(0, 100));
-    const renderer = ImageEffectRenderer.createTemporary(element, shader, options);
+
+    resolveIncludesAsync(shader).then((shader) => {
+        const renderer = ImageEffectRenderer.createTemporary(element, shader, options);
+    });
 });
