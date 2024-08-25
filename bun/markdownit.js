066278331ef2f7be9a9fb32862c4d700a4c7f318
utensil <utensilcandel@gmail.com>
2024-08-25 16:28:07 +0800

    test hybrid markdown with forester markup

--- bun/markdownit.js
+++ bun/markdownit.js
@@ -2,13 +2,14 @@
 // for advanced usage, see https://github.com/markdown-it/markdown-it/blob/master/support/demo_template/index.mjs
 import markdownit from 'markdown-it'
 
-const md = markdownit();
+const md = markdownit({html: true});
 
 const markdownit_tags = document.querySelectorAll('.markdownit-raw');
 console.log(markdownit_tags);
 for (let i = 0; i < markdownit_tags.length; i++) {
     const markdownit_tag = markdownit_tags[i];
-    const markdown_source = markdownit_tag.textContent;
+    const markdown_source = markdownit_tag.innerHTML;
+    console.log(markdown_source);
     markdownit_tag.innerHTML = md.render(markdown_source);
     markdownit_tag.classList.remove('markdownit-raw');
 }
