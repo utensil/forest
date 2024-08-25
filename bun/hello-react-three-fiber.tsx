ebcb3211599fff238d952009b6cb4c79b608849f
utensil <utensilcandel@gmail.com>
2024-08-25 15:27:52 +0800

    Test Billboard, CameraControls from drei

--- bun/hello-react-three-fiber.tsx
+++ bun/hello-react-three-fiber.tsx
@@ -1,9 +1,10 @@
 // example from https://github.com/pmndrs/react-three-fiber
-// bun install three react-dom react @react-three/fiber
+// bun install three react-dom react @react-three/fiber @react-three/drei
 import * as THREE from 'three'
 import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Billboard, Text, Plane, CameraControls } from '@react-three/drei'
 
 function Box(props: ThreeElements['mesh']) {
   const ref = useRef<THREE.Mesh>(null!)
@@ -26,10 +27,38 @@ function Box(props: ThreeElements['mesh']) {
 
 createRoot(document.getElementById('r3f-root') as HTMLElement).render(
   <Canvas>
+    <CameraControls />
     <ambientLight intensity={Math.PI / 2} />
     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
     <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
     <Box position={[-1.2, 0, 0]} />
     <Box position={[1.2, 0, 0]} />
+    <Billboard>
+        <Text>hi</Text>
+        <Plane args={[10, 10]}>
+            <shaderMaterial
+                fragmentShader={`varying vec2 vUv;
+
+vec3 colorA = vec3(0.912,0.191,0.652);
+vec3 colorB = vec3(1.000,0.777,0.052);
+
+void main() {
+  // "Normalizing" with an arbitrary value
+  // We'll see a cleaner technique later :)   
+  vec2 normalizedPixel = gl_FragCoord.xy/600.0;
+  vec3 color = mix(colorA, colorB, normalizedPixel.x);
+
+  gl_FragColor = vec4(color,1.0);
+}`}
+                vertexShader={`void main() {
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+}`}
+            />
+        </Plane>
+    </Billboard>
   </Canvas>,
 )
