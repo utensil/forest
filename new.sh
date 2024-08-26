b5c738196c0448da06059958341d222461ad7fc8
utensil <utensilcandel@gmail.com>
2024-08-26 22:02:14 +0800

    Maintain templates by prefix, with a default; remove outdated templates

--- new.sh
+++ new.sh
@@ -2,4 +2,10 @@
 TREE_PREFIX=${1:-uts}
 FILENAME=$(opam exec -- forester new --dest=trees --prefix=$TREE_PREFIX)
 echo $FILENAME
-cat templates/texdef.tree > $FILENAME
+
+# if templates/$TREE_PREFIX.tree exists
+if [ -f templates/$TREE_PREFIX.tree ]; then
+    cat templates/$TREE_PREFIX.tree > $FILENAME
+else
+    cat templates/ag.tree > $FILENAME
+fi
