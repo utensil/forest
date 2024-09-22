50e9f1900aa92c460e899d875bbbe97ef9cb7037
utensil <utensilcandel@gmail.com>
2024-09-22 13:41:15 +0800

    Add diary for 09-19~09-21

--- dev.sh
+++ dev.sh
@@ -12,7 +12,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-watchexec --quiet --no-vcs-ignore --project-origin . --on-busy-update queue --poll 500ms -e tree,tex,bib,css,js,jsx,xsl,ts,tsx,glsl,typ,domain,style,substance,trio.json -w trees -w assets -w tex -w bun --emit-events-to=stdio -- ./build_changed.sh &
+POLL="--poll 500ms" # $POLL
+
+watchexec --quiet --no-vcs-ignore --project-origin . --on-busy-update queue  -e tree,tex,bib,css,js,jsx,xsl,ts,tsx,glsl,typ,domain,style,substance,trio.json -w trees -w assets -w tex -w bun --emit-events-to=stdio -- ./build_changed.sh &
 
 http-server -p 1314 output &
 
