17efa3e1c1cb6fcb78814d320948d610f4973c13
utensil <utensilcandel@gmail.com>
2024-09-23 15:07:27 +0800

    Add: Raymarching in raymarching (sphere tracing)

--- bun/shadertoy.jsx
+++ bun/shadertoy.jsx
@@ -130,9 +130,9 @@ embeded_shaders.forEach((element) => {
             createRoot(element).render(
                 <Canvas>
                   {/* <CameraControls /> */}
-                  <ambientLight intensity={Math.PI / 2} />
+                  {/* <ambientLight intensity={Math.PI / 2} />
                   <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                  <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                  <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
                   <Billboard>
                       <ShaderPlane fragmentShader={shader}/>
                   </Billboard>
