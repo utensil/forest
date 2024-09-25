34cad4a3bad727ddaec81b7759eaa397a0ab0f7a
utensil <utensilcandel@gmail.com>
2024-09-25 15:39:33 +0800

    Lint bun_build.js too

--- bun_build.js
+++ bun_build.js
@@ -1,45 +1,43 @@
-import Bun from 'bun';
+import Bun from 'bun'
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 // console.log(args);
 
 const wgslLoader = {
-    name: "wgsl",
+    name: 'wgsl',
     async setup(build) {
-      const { transpileWGSL } = await import("@use-gpu/shader/wgsl");
-  
-      // when a .wgsl file is imported...
-      build.onLoad({ filter: /\.(wgsl)$/ }, async (args) => {
-        // read and parse the file
-        const source = await Bun.file(args.path).text();
-        const exports = transpileWGSL(source, args.path);
+        const { transpileWGSL } = await import('@use-gpu/shader/wgsl')
 
-        // console.log(exports);
-  
-        // and returns it as a module
-        return {
-          contents: exports,
-          // exports,
-          loader: "js", // special loader for JS objects
-        };
-      });
+        // when a .wgsl file is imported...
+        build.onLoad({ filter: /\.(wgsl)$/ }, async (args) => {
+            // read and parse the file
+            const source = await Bun.file(args.path).text()
+            const exports = transpileWGSL(source, args.path)
+
+            // console.log(exports);
+
+            // and returns it as a module
+            return {
+                contents: exports,
+                // exports,
+                loader: 'js', // special loader for JS objects
+            }
+        })
     },
-  };
+}
 
 const result = await Bun.build({
-    entrypoints:[args[0]],
-    outdir:"./output",
+    entrypoints: [args[0]],
+    outdir: './output',
     target: 'browser',
-    plugins:[
-        wgslLoader
-    ]
-});
+    plugins: [wgslLoader],
+})
 
 if (!result.success) {
-    console.error("Build failed");
+    console.error('Build failed')
     for (const message of result.logs) {
         // Bun will pretty print the message object
-        console.error(message);
+        console.error(message)
     }
 }
