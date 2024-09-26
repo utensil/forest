e1b6c1c9bdcc750df9ee18a6b0f58a471eb15cc0
utensil <utensilcandel@gmail.com>
2024-09-26 15:12:53 +0800

    Try `run_script` for Rhai

--- bun/rhai.js
+++ bun/rhai.js
@@ -1,11 +1,21 @@
-import init, { compile_script } from '../lib/rhaiscript/pkg'
+import init, { compile_script, run_script } from '../lib/rhaiscript/pkg'
 
 await init()
 
-console.log(compile_script(`\
+const script = `\
 fn run(a) {
     let b = a + 1;
     print("Hello world! a = " + a);
 }
 run(10);
-`))
+`
+
+console.log(compile_script(script))
+
+console.log(
+    run_script(
+        script,
+        (s) => console.log(s),
+        (S) => console.debug(s),
+    ),
+)
