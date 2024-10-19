a564798a4c28109f3e0b48c72009a8b930436ec9
utensil <utensilcandel@gmail.com>
2024-10-19 23:17:15 +0800

    Fix requring init

--- config.lua
+++ config.lua
@@ -3,6 +3,11 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+local current_file = debug.getinfo(1, "S").source:sub(2)
+local current_dir = current_file:match("(.*/)")
+package.path = package.path .. ';' .. current_dir .. '?.lua'
+
 require "init"
 
 lvim.plugins = {
