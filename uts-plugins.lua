1c877c2f942e788de0cebd712a8bf2b33618ea47
utensil <utensilcandel@gmail.com>
2024-11-01 22:52:30 +0800

    Try add CodeCompanion from Zed

--- uts-plugins.lua
+++ uts-plugins.lua
@@ -351,6 +351,36 @@ local plugins = {
         },
     },
     {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+            "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+            { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+            "echasnovski/mini.nvim",
+        },
+        config = function()
+            require("codecompanion").setup {
+                display = {
+                    diff = {
+                        provider = "mini_diff",
+                    },
+                },
+            }
+        end,
+        keys = {
+            -- use <C-z> to trigger CodeCompanion for both n and v
+            { "<localleader>z", mode = "n", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion - Inline" },
+            { "<localleader>z", mode = "v", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion - Inline" },
+            { "<C-a>", mode = "n", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion - Actions" },
+            { "<C-a>", mode = "v", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion - Actions" },
+            { "<localleader>a", mode = "n", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CodeCompanion - Chat Toggle" },
+            { "<localleader>a", mode = "v", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CodeCompanion - Chat Toggle" },
+            { "ga", mode = "v", "<cmd>CodeCompanionChat Add<cr>", desc = "CodeCompanion - Chat Add" },
+        },
+    },
+    {
         "zbirenbaum/copilot-cmp",
         after = {
             "copilot.vim",
