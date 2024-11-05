5f488ba0f5fc52f2b226447a6c5fb62b6195918d
utensil <utensilcandel@gmail.com>
2024-11-05 14:34:13 +0800

    Add command palette and session resume

--- uts-plugins.lua
+++ uts-plugins.lua
@@ -937,6 +937,167 @@ local plugins = {
         end,
     },
     -- {
+    --     "rcarriga/nvim-notify",
+    --     keys = {
+    --         {
+    --             "<leader>un",
+    --             function()
+    --                 require("notify").dismiss { silent = true, pending = true }
+    --             end,
+    --             desc = "Dismiss All Notifications",
+    --         },
+    --     },
+    --     opts = {
+    --         stages = "static",
+    --         timeout = 3000,
+    --         max_height = function()
+    --             return math.floor(vim.o.lines * 0.75)
+    --         end,
+    --         max_width = function()
+    --             return math.floor(vim.o.columns * 0.75)
+    --         end,
+    --         on_open = function(win)
+    --             vim.api.nvim_win_set_config(win, { zindex = 100 })
+    --         end,
+    --     },
+    --     init = function()
+    --         vim.notify = require "notify"
+    --     end,
+    -- },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        any = {
+                            { find = "%d+L, %d+B" },
+                            { find = "; after #%d+" },
+                            { find = "; before #%d+" },
+                        },
+                    },
+                    view = "mini",
+                },
+            },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+            },
+        },
+        keys = {
+            { "<leader>sn", "", desc = "+noice" },
+            {
+                "<S-Enter>",
+                function()
+                    require("noice").redirect(vim.fn.getcmdline())
+                end,
+                mode = "c",
+                desc = "Redirect Cmdline",
+            },
+            {
+                "<leader>snl",
+                function()
+                    require("noice").cmd "last"
+                end,
+                desc = "Noice Last Message",
+            },
+            {
+                "<leader>snh",
+                function()
+                    require("noice").cmd "history"
+                end,
+                desc = "Noice History",
+            },
+            {
+                "<leader>sna",
+                function()
+                    require("noice").cmd "all"
+                end,
+                desc = "Noice All",
+            },
+            {
+                "<leader>snd",
+                function()
+                    require("noice").cmd "dismiss"
+                end,
+                desc = "Dismiss All",
+            },
+            {
+                "<leader>snt",
+                function()
+                    require("noice").cmd "nick"
+                end,
+                desc = "Noice Picker (Telescope/FzfLua)",
+            },
+            {
+                "<c-f>",
+                function()
+                    if not require("noice.lsp").scroll(4) then
+                        return "<c-f>"
+                    end
+                end,
+                silent = true,
+                expr = true,
+                desc = "Scroll Forward",
+                mode = { "i", "n", "s" },
+            },
+            {
+                "<c-b>",
+                function()
+                    if not require("noice.lsp").scroll(-4) then
+                        return "<c-b>"
+                    end
+                end,
+                silent = true,
+                expr = true,
+                desc = "Scroll Backward",
+                mode = { "i", "n", "s" },
+            },
+        },
+        config = function(_, opts)
+            -- HACK: noice shows messages from before it was enabled,
+            -- but this is not ideal when Lazy is installing plugins,
+            -- so clear the messages in this case.
+            if vim.o.filetype == "lazy" then
+                vim.cmd [[messages clear]]
+            end
+            require("noice").setup(opts)
+        end,
+    },
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        opts = {
+            -- add any custom options here
+        },
+        keys = {
+            {
+                "<leader>rs",
+                function()
+                    require("persistence").load()
+                end,
+                "Load the session for the current working directory",
+            },
+            {
+                "<leader>rS",
+                function()
+                    require("persistence").select()
+                end,
+                "Select a session to load",
+            },
+        },
+    },
+    -- {
     --     "ThePrimeagen/harpoon",
     --     branch = "harpoon2",
     --     dependencies = { "nvim-lua/plenary.nvim" },
