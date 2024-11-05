a53f9cbffda6586d8b5bfe1f4582325e07e46165
utensil <utensilcandel@gmail.com>
2024-11-05 13:12:42 +0800

    Make blink play well with Copilot

--- uts-plugins.lua
+++ uts-plugins.lua
@@ -19,63 +19,85 @@ local plugins = {
     },
     -- { "saghen/blink.compat" },
     -- { "hrsh7th/cmp-emoji" },
-    -- {
-    --     "saghen/blink.cmp",
-    --     lazy = false, -- lazy loading handled internally
-    --     -- optional: provides snippets for the snippet source
-    --     dependencies = "rafamadriz/friendly-snippets",
-    --
-    --     -- use a release tag to download pre-built binaries
-    --     version = "v0.*",
-    --     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    --     -- build = 'cargo build --release',
-    --     -- If you use nix, you can build from source using latest nightly rust with:
-    --     -- build = 'nix run .#build-plugin',
-    --
-    --     ---@module 'blink.cmp'
-    --     ---@type blink.cmp.Config
-    --     opts = {
-    --         -- 'default' for mappings similar to built-in completion
-    --         -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    --         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    --         -- see the "default configuration" section below for full documentation on how to define
-    --         -- your own keymap.
-    --         keymap = { preset = "super-tab" },
-    --
-    --         highlight = {
-    --             -- sets the fallback highlight groups to nvim-cmp's highlight groups
-    --             -- useful for when your theme doesn't support blink.cmp
-    --             -- will be removed in a future release, assuming themes add support
-    --             use_nvim_cmp_as_default = true,
-    --         },
-    --         -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    --         -- adjusts spacing to ensure icons are aligned
-    --         nerd_font_variant = "mono",
-    --
-    --         -- experimental auto-brackets support
-    --         accept = { auto_brackets = { enabled = true } },
-    --
-    --         -- experimental signature help support
-    --         trigger = { signature_help = { enabled = true } },
-    --
-    --         sources = {
-    --             completion = {
-    --                 enabled_providers = { "lsp", "path", "snippets" }, -- , "emoji" }, -- , 'buffer' },
-    --             },
-    --         },
-    --         -- providers = {
-    --         --     -- create provider
-    --         --     emoji = {
-    --         --         name = "emoji", -- IMPORTANT: use the same name as you would for nvim-cmp
-    --         --         module = "blink.compat.source",
-    --         --         opts = {
-    --         --             -- this table is passed directly to the proxied completion source
-    --         --             -- as the `option` field in nvim-cmp's source config
-    --         --         },
-    --         --     },
-    --         -- },
-    --     },
-    -- },
+    {
+        "saghen/blink.cmp",
+        lazy = false, -- lazy loading handled internally
+        -- optional: provides snippets for the snippet source
+        dependencies = "rafamadriz/friendly-snippets",
+        -- use a release tag to download pre-built binaries
+        version = "v0.*",
+        -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' for mappings similar to built-in completion
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            -- see the "default configuration" section below for full documentation on how to define
+            -- your own keymap.
+            keymap = {
+                ["<C-q>"] = { "show", "hide" },
+                ["<Up>"] = { "select_prev", "fallback" },
+                ["<Down>"] = { "select_next", "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
+                ["<Right>"] = { "accept", "fallback" },
+                ["<Left>"] = { "hide" },
+            },
+
+            highlight = {
+                -- sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- useful for when your theme doesn't support blink.cmp
+                -- will be removed in a future release, assuming themes add support
+                use_nvim_cmp_as_default = true,
+            },
+
+            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- adjusts spacing to ensure icons are aligned
+            nerd_font_variant = "mono",
+
+            -- experimental auto-brackets support
+            accept = { auto_brackets = { enabled = true } },
+
+            -- experimental signature help support
+            trigger = { signature_help = { enabled = true }, show_in_snippet = false },
+
+            sources = {
+                completion = {
+                    enabled_providers = { "path", "snippets", "lsp", "buffer" }, -- , "emoji" }, -- , 'buffer' },
+                },
+            },
+            providers = {
+                snippets = {
+                    enabled = function(ctx)
+                        return ctx ~= nil
+                            and ctx.trigger.kind == vim.lsp.protocol.CompletionTriggerKind.TriggerCharacter
+                    end,
+                },
+            },
+            windows = {
+                autocomplete = {
+                    auto_show = false,
+                    selection = "manual",
+                    draw = "reversed",
+                },
+            },
+            -- providers = {
+            --     -- create provider
+            --     emoji = {
+            --         name = "emoji", -- IMPORTANT: use the same name as you would for nvim-cmp
+            --         module = "blink.compat.source",
+            --         opts = {
+            --             -- this table is passed directly to the proxied completion source
+            --             -- as the `option` field in nvim-cmp's source config
+            --         },
+            --     },
+            -- },
+        },
+    },
 
     -- LSP servers and clients communicate what features they support through "capabilities".
     --  By default, Neovim support a subset of the LSP specification.
