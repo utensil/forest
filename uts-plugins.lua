b2f1b4b71e03e8d7d90cd4c40b0cbb34c09f36bb
utensil <utensilcandel@gmail.com>
2024-11-06 13:07:53 +0800

    Add emoji search, with bonus of LaTeX and Nerd font symbols

--- uts-plugins.lua
+++ uts-plugins.lua
@@ -29,6 +29,50 @@ local plugins = {
     --     }
     -- },
     -- { "hrsh7th/cmp-emoji" },
+    -- {
+    --     "allaman/emoji.nvim",
+    --     version = "1.0.0", -- optionally pin to a tag
+    --     ft = "markdown", -- adjust to your needs
+    --     dependencies = {
+    --         -- util for handling paths
+    --         "nvim-lua/plenary.nvim",
+    --         -- optional for nvim-cmp integration
+    --         -- "hrsh7th/nvim-cmp",
+    --         -- optional for telescope integration
+    --         "nvim-telescope/telescope.nvim",
+    --     },
+    --     opts = {
+    --         -- default is false
+    --         enable_cmp_integration = false,
+    --         -- optional if your plugin installation directory
+    --         -- is not vim.fn.stdpath("data") .. "/lazy/
+    --         -- plugin_path = vim.fn.expand "$HOME/plugins/",
+    --     },
+    --     config = function(_, opts)
+    --         require("emoji").setup(opts)
+    --         -- optional for telescope integration
+    --         local ts = require("telescope").load_extension "emoji"
+    --         vim.keymap.set("n", "<leader>se", ts.emoji, { desc = "[S]earch [E]moji" })
+    --     end,
+    -- },
+    -- {
+    --     "xiyaowong/telescope-emoji.nvim",
+    --     dependencies = {
+    --         "nvim-telescope/telescope.nvim",
+    --     },
+    --     config = function()
+    --         require("telescope").load_extension "emoji"
+    --     end,
+    --     keys = {
+    --         { "<leader>se", "<cmd>Telescope emoji<cr>", desc = "Search Emoji" },
+    --     },
+    -- },
+    {
+        "nvim-telescope/telescope-symbols.nvim",
+        keys = {
+            { "<leader>se", "<cmd>Telescope symbols<cr>", desc = "Search Symbols: emoji, latex" },
+        },
+    },
     {
         "saghen/blink.cmp",
         lazy = false, -- lazy loading handled internally
