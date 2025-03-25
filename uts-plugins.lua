10ee840451b6c6b460aae86fecd1b5e40ce88c4f
utensil <utensilcandel@gmail.com>
2025-03-25 16:23:51 +0800

    Add toggle to conform autoformat

--- uts-plugins.lua
+++ uts-plugins.lua
@@ -638,17 +638,81 @@ local ui_plugins = {
 local lang_plugins = {
     {
         "stevearc/conform.nvim",
+        -- The following is based on https://github.com/stevearc/conform.nvim/issues/192#issuecomment-2573170631
+        -- To be able to disable autoformat
+        lazy = false,
+        keys = {
+            {
+                "<leader>tf",
+                function()
+                    -- If autoformat is currently disabled for this buffer,
+                    -- then enable it, otherwise disable it
+                    if vim.b.disable_autoformat then
+                        vim.cmd "FormatEnable"
+                        vim.notify "Enabled autoformat for current buffer"
+                    else
+                        vim.cmd "FormatDisable!"
+                        vim.notify "Disabled autoformat for current buffer"
+                    end
+                end,
+                desc = "Toggle autoformat for current buffer",
+            },
+            {
+                "<leader>tF",
+                function()
+                    -- If autoformat is currently disabled globally,
+                    -- then enable it globally, otherwise disable it globally
+                    if vim.g.disable_autoformat then
+                        vim.cmd "FormatEnable"
+                        vim.notify "Enabled autoformat globally"
+                    else
+                        vim.cmd "FormatDisable"
+                        vim.notify "Disabled autoformat globally"
+                    end
+                end,
+                desc = "Toggle autoformat globally",
+            },
+        },
         opts = {
+            notify_on_error = false,
+            format_on_save = function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                local disable_filetypes = {} -- { python = false }
+                return {
+                    timeout_ms = 500,
+                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                }
+            end,
             formatters_by_ft = {
                 lua = { "stylua" },
                 bib = { "trim_whitespace" },
             },
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
         },
+        config = function(_, opts)
+            require("conform").setup(opts)
+
+            vim.api.nvim_create_user_command("FormatDisable", function(args)
+                if args.bang then
+                    -- :FormatDisable! disables autoformat for this buffer only
+                    vim.b.disable_autoformat = true
+                else
+                    -- :FormatDisable disables autoformat globally
+                    vim.g.disable_autoformat = true
+                end
+            end, {
+                desc = "Disable autoformat-on-save",
+                bang = true, -- allows the ! variant
+            })
+
+            vim.api.nvim_create_user_command("FormatEnable", function()
+                vim.b.disable_autoformat = false
+                vim.g.disable_autoformat = false
+            end, {
+                desc = "Re-enable autoformat-on-save",
+            })
+        end,
     },
     {
         "kentookura/forester.nvim",
